e5e3dfceddb4de87f4d642100bf391717d138a42
Jonathan Cody <jrcody38@gmail.com>
2019-07-30 15:35:31 +0100

    Code Refactoring

--- WorkflowManager/WorkflowParser.cs
+++ WorkflowManager/WorkflowParser.cs
@@ -9,48 +9,56 @@ namespace WorkflowManager
     {
         public static Workflow ParseInput(string input)
         {
-            List<ExecutionSequence> sequences = new List<ExecutionSequence>();
-            List<WorkflowTask> workflowTasks = new List<WorkflowTask>();
-
+          
+          
             string[] fileRows = input.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
             var name = fileRows[0].Replace("desc", "").Trim();
+            int rowCounter = 1;
+            
+            return new Workflow(name, generateSequences(generateWorkflowTasks(fileRows, ref rowCounter), fileRows, rowCounter));
+        }
+
+        static List<WorkflowTask> generateWorkflowTasks(string[] fileRows, ref int rowCounter)
+        {
+            List<WorkflowTask> workflowTasks = new List<WorkflowTask>();
             string currentRowContent = "";
             string parameters = "";
-            int counter1 = 1;
-            int counter2 = 1;
-
 
             while (currentRowContent.ToLower().Trim() != "end")
             {
-                currentRowContent = fileRows[counter1];
+                currentRowContent = fileRows[rowCounter];
 
-                if(currentRowContent.ToLower().Trim() == "end")
+                if (currentRowContent.ToLower().Trim() == "end")
                 {
                     continue;
                 }
 
                 string[] splitRow = currentRowContent.Split('=');
-                string[] rowValues = splitRow[1].Split(' ').Where( x => !string.IsNullOrEmpty(x)).ToArray();
-
+                string[] rowValues = splitRow[1].Split(' ').Where(x => !string.IsNullOrEmpty(x)).ToArray();
                 var task = new WorkflowTask();
                 task.Id = currentRowContent.Split('=')[0].Replace("=", "").Trim();
                 task.Name = rowValues[0].Split(' ')[0].Trim();
 
-                for (int x =1; x < rowValues.Length; x++)
+                for (int x = 1; x < rowValues.Length; x++)
                 {
                     var value = rowValues[x].Trim();
                     parameters += value + " ";
                 }
 
                 task.Parameter = parameters.TrimEnd();
-
                 workflowTasks.Add(task);
                 parameters = "";
-                counter1++;
+                rowCounter++;
             }
 
+            return workflowTasks;
+        }
+        static List<ExecutionSequence> generateSequences(List<WorkflowTask> workflowTasks, string[] fileRows, int rowCounter)
+        {
+            List<ExecutionSequence> sequences = new List<ExecutionSequence>();
+            int sequenceCounter = 1;
 
-            for (int x = counter1 + 1; x < fileRows.Length; x++)
+            for (int x = rowCounter + 1; x < fileRows.Length; x++)
             {
                 string[] sequenceIds = fileRows[x].Split(new string[] { "->" }, StringSplitOptions.None);
                 List<WorkflowTask> tempTasks = new List<WorkflowTask>();
@@ -59,14 +67,13 @@ namespace WorkflowManager
                 {
                     tempTasks.Add(workflowTasks.Where(y => y.Id == id).FirstOrDefault());
                 }
-                sequences.Add(new ExecutionSequence(tempTasks, "Sequence_" + counter2));
-                counter1++;
-                counter2++;
+                sequences.Add(new ExecutionSequence(tempTasks, "Sequence_" + sequenceCounter));
+                sequenceCounter++;
 
             }
 
-            return new Workflow(name, sequences);
+            return sequences;
+           
         }
-
     }
 }
